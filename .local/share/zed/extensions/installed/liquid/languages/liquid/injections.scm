e99559a1cf8d88160ecc2aaa78cd1bd49958bc73
Bruno Martinez <bmartinez.fullstack@gmail.com>
2026-03-12 22:08:04 -0300

    feat: include zed extensions

---
+++ .local/share/zed/extensions/installed/liquid/languages/liquid/injections.scm
@@ -0,0 +1,29 @@
+((template_content) @injection.content
+  (#set! injection.language "html")
+  (#set! injection.combined))
+
+(javascript_statement
+  (js_content) @injection.content
+  (#set! injection.language "javascript")
+  (#set! injection.combined))
+
+(schema_statement
+  (json_content) @injection.content
+  (#set! injection.language "json")
+  (#set! injection.combined))
+
+(style_statement
+  (style_content) @injection.content
+  (#set! injection.language "css")
+  (#set! injection.combined))
+
+(stylesheet_statement
+  (stylesheet_content) @injection.content
+  (#set! injection.language "css")
+  (#set! injection.combined))
+
+((front_matter) @injection.content
+  (#set! injection.language "yaml"))
+
+((comment) @injection.content
+  (#set! injection.language "comment"))
